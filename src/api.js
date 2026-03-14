84b7a2ff9a0e198d4fc35a4475510d291034aab2
Muhammad Faizan <faizan.brw.pk@gmail.com>
2026-03-15 03:02:44 +0500

    c

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "https://backend-test-deploy-lm9h.onrender.com",
 });
 
 export default api;
